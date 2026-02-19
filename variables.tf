3cf5f59ea125ed9e3e2a462a1f8fc19d4f5ed77d
gokulpavan-999 <pavanthota480@gmail.com>
2026-02-19 15:08:31 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "pavandevops.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z08654702BW8GGH0J30B"
+  description = "description"
+}
